3e3c2a41205d4fcc504b79d9bc02562e6a8b0ace
Milan V. Kecojević <staog@gmx.com>
2017-02-27 22:41:56 +0100

    Update js file

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
-// Creating new array 'tasks' if there isn't one
-// If there is array 'tasks' in localStorage, get data from it
 if(!localStorage.tasks){
   var tasks = [];
 } else {
   var tasks = JSON.parse(localStorage.tasks);
 }
 
-// Creating global variables
+var inputText = document.getElementById('field');
 var addBtn = document.getElementsByTagName('button')[0];
 var deleteBtn = document.getElementsByTagName('button')[1];
-var inputText = document.getElementById('field');
 var insertToDiv = document.getElementById('insert');
 
-
-
-// Adding event listeners to both mouse click and 'enter' button
 addBtn.addEventListener('click', submitTask);
 inputText.addEventListener('keydown', function (e) {
   if (e.keyCode === 13 && inputText.value !== '') {
@@ -22,8 +16,6 @@ inputText.addEventListener('keydown', function (e) {
   }
 })
 
-// Taking input value, saving it in object 'taskToSave', pushing it to
-// array 'tasks' and calling function 'showNewTask'
 function submitTask() {
   var userInput = inputText.value;
   var getDate = new Date();
@@ -35,34 +27,30 @@ function submitTask() {
   }
   tasks.push(taskToSave);
   localStorage.tasks = JSON.stringify(tasks);
+
   showNewTask();
+
 }
 
-// Making empty string 'showPanel', using for loop to go through all the tasks,
-// creating new div element for every task that we have, adding it to string
-// 'showPanel' and inserting all newly created div elements into HTML document
 function showNewTask() {
   var showPanel = '';
   for (var i = 0; i < tasks.length; i++) {
-    showPanel += `<div class="container">
-                    <div class="col-lg-8 col-lg-offset-2">
-                      <div class="panel panel-default">
-                        <div class="panel-heading">Task No. ${i + 1}</div>
-                        <div class="panel-body">
-                          <h3>${tasks[i].task}</h3>
-                          <h5>${tasks[i].date}</h5>
-                        </div>
-                       <div class="panel-footer">
-                          <button class="btn btn-danger pull-right deleteBtns" type="button" name="button">Delete</button>
-                        <div class="clearfix"></div>
-                       </div>
-                       </div>
+    showPanel += `<div class="col-lg-4">
+                    <div class="panel panel-default">
+                      <div class="panel-heading">Task No. ${i + 1}</div>
+                      <div class="panel-body">
+                        <h3>${tasks[i].task}</h3>
+                        <h5>${tasks[i].date}</h5>
+                      </div>
+                      <div class="panel-footer">
+                        <button class="btn btn-danger pull-right deleteBtns" type="button" name="button">Delete</button>
+                      <div class="clearfix"></div>
+                      </div>
                     </div>
                   </div>`
   }
   insertToDiv.innerHTML = showPanel;
   inputText.focus();
-  // Adding event listeners to all newly created 'delete' buttons
   var dltBtns = document.getElementsByClassName('deleteBtns');
   for (let i = 0; i < dltBtns.length; i++) {
     dltBtns[i].addEventListener('click', function () {
@@ -71,11 +59,18 @@ function showNewTask() {
   }
 }
 
-// Deleting specific task using 'x' argument (just a reference to 'i' in for loop)
 function deleteTask(x) {
-      tasks.splice(x, 1);
-      localStorage.tasks = JSON.stringify(tasks);
-      showNewTask();
+  tasks.splice(x, 1);
+  localStorage.tasks = JSON.stringify(tasks);
+  showNewTask();
+}
+
+deleteBtn.addEventListener('click', deleteAll)
+
+function deleteAll() {
+  tasks.splice(0);
+  localStorage.tasks = JSON.stringify(tasks);
+  showNewTask();
 }
 
 showNewTask();
